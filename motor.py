27349381a10594359a34b7332ebd38e43f5e5f7f
Josh Ashby <joshuaashby@joshashby.com>
2009-07-08 18:59:57 -0600

    replaced code with a more basic version that i will build off of, with luck this will get BOBs newly finished motor controllers working and driving from tele-op.

---
+++ motor.py
@@ -0,0 +1,9 @@
+  #!/usr/bin/env python
+  import serial #import the serial comunication library
+  usbport = '/dev/ttyUSB0'
+  ser = serial.Serial(usbport, 9600, timeout=1)
+  def move(motor, angle):
+    ser.write(chr(255))
+    ser.write(chr(motor))
+    ser.write(chr(speed))
+    ser.write(chr(fb))
