78e7274bf76dc15563fa82d71e4e0438cb02a441
Josh Ashby <joshuaashby@joshashby.com>
2010-02-13 12:28:52 -0700

    new branch with new C code, removed/got rid of arduino libs

--- motor.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-import serial #import the serial comunication library
-usbport = '/dev/ttyUSB0'
-ser = serial.Serial(usbport, 9600, timeout=1)
-def move(motor, speed, fb):
-    ser.write(chr(255))
-    ser.write(chr(motor))
-    ser.write(chr(speed))
-    ser.write(chr(fb))
